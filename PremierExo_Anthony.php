a83ba73f0dbf69d55092757e435117e809d16f56
Squalalah <riquezoune@gmail.com>
2021-06-07 16:36:25 +0200

    Ajout de mes exercices

---
+++ PremierExo_Anthony.php
@@ -0,0 +1,69 @@
+<?php
+
+echo 'EXERCICE 1'. PHP_EOL;
+
+$a = 1;
+if (is_numeric($a)) {
+    echo "1 est Positif". PHP_EOL;
+}
+else {
+    echo "1 est négatif". PHP_EOL;
+}
+
+echo 'Exercice 2'. PHP_EOL;
+
+$b = rand(-50,50);
+
+if(is_numeric($b)) {
+    printf("%d est positif". PHP_EOL, $b);
+}
+else printf("%d est négatif". PHP_EOL, $b);
+if (!$b) printf("%d est une valeur nulle (0)". PHP_EOL, $b);
+
+echo "Exercice 3". PHP_EOL;
+
+echo 'merci de saisir une valeur : ';
+
+$handle = fopen('php://stdin', 'r');
+$value = fgets($handle);
+
+if(strlen($value) > 30) {
+    echo "La phrase est trop grande". PHP_EOL;
+}
+else echo "Merci pour ce commentaire constructif". PHP_EOL;
+
+echo "Exercice 4". PHP_EOL;
+
+echo 'Merci de saisir un entier positif : '. PHP_EOL;
+
+$handle = fopen('php://stdin', 'r');
+$result = trim(fgets($handle));
+// ---
+// $result = readline('Merci de saisir un entier positif : ');
+// is_numeric('.4') => false donc pas besoin de se soucis du retour 0 possible par la fonction strpos.
+if (is_numeric($result) && !strpos($result, '.')) { // Si c'est un numérique sans '.' (donc un entier).
+    $intVal = intval($result);
+
+    echo 'Merci de saisir un entier positif : ' . PHP_EOL;
+    $handleAux = fopen('php://stdin', 'r');
+    $resultAux = trim(fgets($handleAux));
+
+    if (is_numeric($resultAux) && !strpos($resultAux, '.')) { // Si c'est un numérique sans '.' (donc un entier).
+        $intValDeuxieme = intval($resultAux);
+
+        printf("Le plus grand des deux est %d" . PHP_EOL, max($intVal, $intValDeuxieme));
+        echo 'Merci de saisir un entier positif : ' . PHP_EOL;
+        $handleAuxBis = fopen('php://stdin', 'r');
+        $resultAuxBis = trim(fgets($handleAuxBis));
+
+        if (is_numeric($resultAuxBis) && !strpos($resultAuxBis, '.')) { // Si c'est un numérique sans '.' (donc un entier).
+            $intValTroisieme = intval($resultAuxBis);
+
+            printf("Le plus grand des trois est %d" . PHP_EOL, max($intVal, $intValDeuxieme, $intValTroisieme));
+        }
+    }
+}
+
+
+
+?>
---
+++ pendu_Anthony.php
@@ -0,0 +1,58 @@
+<?php
+
+$answer = "mystere";
+
+echo "Veuillez saisir le mot à trouver";
+
+$handle = fopen('php://stdin', 'r');
+$result = trim(fgets($handle));
+
+if(is_numeric($result))
+{
+    echo 'Qu\'est ce que vous ne comprennez pas dans "saisir un MOT" ?'. PHP_EOL;
+}
+else {
+    printf('Mot choisi : %s', $result);
+    echo'Mot à deviner : mystere'.PHP_EOL;
+    if($result === 'mystere')
+    {
+        echo 'Bravo, vous avez gagné !'. PHP_EOL;
+    }
+    else {
+        echo 'Perdu ! (franchement, ca craint un peu vu que vous connaissez la réponse)'. PHP_EOL;
+    }
+}
+
+echo 'Bon allez, on retente mais en plus dur'.PHP_EOL;
+$array = array('mystere', 'boule', 'enfant', 'demenagement');
+$answerAux = array_rand( $array, 1);
+echo "Veuillez saisir le mot à trouver";
+$handleAux = fopen('php://stdin', 'r');
+$resultAux = trim(fgets($handleAux));
+
+
+if(is_numeric($resultAux))
+{
+    echo 'Qu\'est ce que vous ne comprennez pas dans "saisir un MOT" ?'. PHP_EOL;
+}
+else {
+    printf('Mot choisi : %s', $resultAux);
+    printf('Mot à deviner : %s'.PHP_EOL, $array[$answerAux]);
+    if($resultAux === $array[$answerAux])
+    {
+        echo 'Bravo, vous avez gagné !'. PHP_EOL;
+    }
+    else {
+        echo 'Perdu ! (bon ca craint moins car on ne peut connaitre la réponse à l\'avance )'. PHP_EOL;
+    }
+}
+
+
+/*
+ *
+ *
+ */
+
+
+?>
+
