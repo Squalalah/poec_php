a1f421b15c3b86e325c43635b3fb3702c4321bca
Squalalah <riquezoune@gmail.com>
2021-06-08 20:10:35 +0200

    Completion pendu + ajout partiel du MVC

---
+++ Anthony/VUE/pendu_html.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+
+</head>
+
+
+<body>
+
+<form action="#" method="POST">
+    <label for="proposition">Votre proposition:</label><br>
+    <input type="text" id="proposition" name="proposition" value=""><br>
+    <input type="submit" value="Submit">
+    <input type="submit" value="Destroy Session" name="destroy" value="destroy">
+</form>
+<br />
+<br />
+<form action="#" method="POST">
+    <label for="ajoutMot">Mot à ajouer:</label><br>
+    <input type="text" id="ajoutMot" name="ajoutMot" value=""><br>
+    <input type="submit" value="Ajouter le mot">
+</form>
+
+</body>
+
+</html>
+
+
+<?php
+include("../CONTROLEUR/penduCONTROL.php");
+?>
+
---
+++ Anthony/index.php
@@ -0,0 +1,5 @@
+<?php
+
+header("Location: VUE/pendu_html.php", TRUE, 301);
+exit();
+?>
