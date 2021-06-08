79e756be176229b2799fab7f6c4ca6377abdcb92
Squalalah <riquezoune@gmail.com>
2021-06-08 17:50:31 +0200

    Ajout fichier html

--- Anthony/pendu_Anthony.php
+++ Anthony/pendu_Anthony.php
@@ -1,35 +1,4 @@
 <?php
-/*
- *
- *
- *
- * Exercice 1
- *
- *
- *
- */
-$answer = "mystere";
-
-echo "Veuillez saisir le mot à trouver";
-
-$handle = fopen('php://stdin', 'r');
-$result = trim(fgets($handle));
-
-if(is_numeric($result))
-{
-    echo 'Qu\'est ce que vous ne comprennez pas dans "saisir un MOT" ?'. PHP_EOL;
-}
-else {
-    printf('Mot choisi : %s', $result);
-    echo'Mot à deviner : mystere'.PHP_EOL;
-    if($result === 'mystere')
-    {
-        echo 'Bravo, vous avez gagné !'. PHP_EOL;
-    }
-    else {
-        echo 'Perdu ! (franchement, ca craint un peu vu que vous connaissez la réponse)'. PHP_EOL;
-    }
-}
 
 /*
  *
@@ -42,28 +11,97 @@ else {
  */
 
 echo 'Bon allez, on retente mais en plus dur'.PHP_EOL;
-$array = array('mystere', 'boule', 'enfant', 'demenagement');
-$answerAux = array_rand( $array, 1);
-echo "Veuillez saisir le mot à trouver";
-$handleAux = fopen('php://stdin', 'r');
-$resultAux = trim(fgets($handleAux));
+$fileContent = file_get_contents('mots.txt');
+$array = explode(',', $fileContent);
+$chance = 5;
+$answerAux = $array[array_rand( $array, 1)];
+$arrayLastLetters = str_split($answerAux);
+$stringReponseFound= [];
+$letttersAlreadyUsed = '';
 
-
-if(is_numeric($resultAux))
+for ($i=0; $i<strlen($answerAux); $i++)
 {
-    echo 'Qu\'est ce que vous ne comprennez pas dans "saisir un MOT" ?'. PHP_EOL;
+    array_push($stringReponseFound, '_');
 }
-else {
-    printf('Mot choisi : %s', $resultAux);
-    printf('Mot à deviner : %s'.PHP_EOL, $array[$answerAux]);
-    if($resultAux === $array[$answerAux])
+$found = false;
+$handleAux = fopen('php://stdin', 'r');
+echo 'Voulez vous ajouter un mot au fichier ? y pour oui';
+$fileChoice = trim(fgets($handleAux));
+
+if($fileChoice == 'y')
+{
+    $stop = false;
+    echo 'Quel mot voulez vous ajouter ? STOP pour arrêter'. PHP_EOL;
+    $wordChoice = trim(fgets($handleAux));
+    while(!$stop)
     {
-        echo 'Bravo, vous avez gagné !'. PHP_EOL;
-    }
-    else {
-        echo 'Perdu ! (bon ca craint moins car on ne peut connaitre la réponse à l\'avance )'. PHP_EOL;
+        if(!preg_match("/[A-Za-z0-9]+/", $wordChoice))
+        {
+            echo 'Merci d\'entrer un mot valide';
+            continue;
+        }
+        file_put_contents('mots.txt', ','.$wordChoice, FILE_APPEND);
+        echo 'Fichier ajouté, voulez vous continuer ? STOP pour arrêter';
+        $wordChoice = trim(fgets($handleAux));
+        if($wordChoice == "STOP") $stop = true;
+
     }
 }
+
+
+
+while(!$found)
+{
+    echo "Veuillez saisir le mot à trouver";
+    $resultAux = trim(fgets($handleAux));
+    $isWord = false;
+    $try = false;
+    if(strlen($resultAux) > 1) $isWord = true;
+    if(!preg_match("/[A-Za-z0-9]+/", $resultAux))
+    {
+        echo 'Merci d\'entrer une vraie valeur (a-z, A-Z)';
+        continue;
+    }
+    if($resultAux === $answerAux) {
+        $found = true;
+        $try = true;
+    }
+    elseif(!$isWord)  {
+        $position = array_search($resultAux, $arrayLastLetters);
+        if(array_search($resultAux, $stringReponseFound) !== false)
+        {
+            echo 'Vous avez déjà proposé cette lettre';
+            continue;
+        }
+        if(strpos($letttersAlreadyUsed, $resultAux) === false)
+        {
+            $letttersAlreadyUsed .= $resultAux;
+        }
+        while($position !== false) {
+            $try = true;
+            $stringReponseFound[$position] = $resultAux;
+            unset($arrayLastLetters[$position]);
+            echo 'Vous avez trouvé une lettre';
+            $position = array_search($resultAux, $arrayLastLetters);
+        }
+        if(implode($stringReponseFound) == $answerAux)
+        {
+            $found = true;
+        }
+    }
+    if(!$try){
+    $chance--;
+    echo 'Vous n\'avez pas reussit à trouver de mots/lettres, reessayez !';
+    }
+    if($found) {
+        echo 'Vous avez gagné, la réponse était bien '. $answerAux . PHP_EOL;
+    }
+    echo 'Reponse à trouver : '. $answerAux. PHP_EOL;
+    echo 'Votre proposition : '. $resultAux. PHP_EOL;
+    echo 'toutes les lettres tentés ' . $letttersAlreadyUsed. PHP_EOL;
+    echo 'Vos lettres déjà trouvés : '. implode($stringReponseFound);
+}
+
 
 
 /*
