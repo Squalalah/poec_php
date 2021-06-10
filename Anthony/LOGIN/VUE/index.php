cce1d47816b8d01aa5f35929204c4eabd0f79b71
Squalalah <riquezoune@gmail.com>
2021-06-10 11:03:28 +0200

    ajout derniers exo additionnel (login page)

---
+++ Anthony/LOGIN/VUE/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+
+</head>
+
+
+<body>
+
+<form action="#" method="POST">
+    <label for="email">Votre email:</label><br>
+    <input type="text" id="email" name="email" value="" placeholder="blabla@domaine.fr"><br>
+    <label for="password">Votre mot de passe: </label><br>
+    <input type="password" id="password" name="password"><br>
+    <input type="submit" value="Submit">
+</form>
+
+</body>
+
+</html>
+
+
+<?php
+include("../CONTROLEUR/loginCONTROL.php");
+?>
+
