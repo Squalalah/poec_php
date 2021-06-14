230d3f6ae67a8821690ce4398503fd0b53436a98
Squalalah <riquezoune@gmail.com>
2021-06-14 12:12:53 +0200

    Rajout modif manquante formInsert.php

---
+++ Anthony/DEMO_COURS/formInsert.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<?php
+if(isset($_GET['message'])) {
+    echo "insertion effectué";
+}
+?>
+<html lang="fr">
+  <head>
+      <meta charset="UTF-8">
+      <title>Title</title>
+  </head>
+  <body>
+    <form method="POST" action="CONTROLEUR/ajoute.php">
+      Id:<input type="number" name="id" size="5" value="" maxlength="4">
+      <br>
+      Libellé:<input type="text" name="lib" size="40" value="" maxlength="35">
+      <br>
+      Quantité:<input type="number" name="qte" size="5" value="" maxlength="4">
+      <br>
+      <input type="submit" value="Envoyer" name="envoyer">
+    </form>
+  </body>
+</html>
